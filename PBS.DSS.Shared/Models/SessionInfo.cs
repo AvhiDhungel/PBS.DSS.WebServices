767655399dd1dcc00190db5788046ffc87488495
Abhinav Dhungel <abhinavd@pbssystems.com>
2024-09-19 16:02:22 -0600

    Banner

---
+++ PBS.DSS.Shared/Models/SessionInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace PBS.DSS.Shared.Models
+{
+    public class SessionInfo
+    {
+        public string SerialNumber { get; set; } = string.Empty;
+        public string Banner { get; set; } = string.Empty;
+    }
+}
