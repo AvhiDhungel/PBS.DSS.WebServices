618a24bb43a3c3e915984c3a66db05a7e39e2e29
Abhinav Dhungel <abhinavd@pbssystems.com>
2024-09-23 12:35:34 -0600

    Appt Reschedule Link

---
+++ PBS.DSS.Shared/Utilities.cs
@@ -0,0 +1,9 @@
+﻿namespace PBS.DSS.Shared
+{
+    public class Utilities
+    {
+        private static string C_WebAppointmentsURL = "https://webappointments.pbssystems.com";
+
+        public static string GetWebAppointmentsURL() => C_WebAppointmentsURL;
+    }
+}
